f825cbb9f03559b6e4906ddbed2aafee0dcd9dc4
Luiz Gonçalves <dev.luizh@gmail.com>
2020-04-08 15:09:03 -0300

    resize photos and fix avatar card

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="column items-center avatar-width">
     <q-avatar size="100px">
-      <a href="{{ scheduledOnceUrl }}" target="_blank"><img :src="photoUrl" :alt="name"></a>
+      <a :href="scheduledOnceUrl" target="_blank"><img :src="photoUrl" :alt="name"></a>
     </q-avatar>
     <h1 class="text-h5 text-grey-8 q-ma-none q-mt-md text-center text-weight-bold">{{ name }}</h1>
     <p class="text-body2 text-grey-7 q-ma-none text-center">{{ shortDescription }}</p>
